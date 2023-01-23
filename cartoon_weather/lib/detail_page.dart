6e4ec0a4cd76aa8afaf189c3229b50be2c1ec346
vladcto <razrab.ytka@gmail.com>
2023-01-24 02:10:00 +0300

    Сделал показ восхода/захода солнца. Пока без теней. Пришлось отключить Hero переход, т.к. конфликтует с разметкой.

--- cartoon_weather/lib/detail_page.dart
+++ cartoon_weather/lib/detail_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
   static const double cardBorderWidth = 4;
+  static const double headerHeight = 148;
 
   const DetailPage({Key? key}) : super(key: key);
 
@@ -33,7 +34,7 @@ class DetailPage extends StatelessWidget {
         child: Stack(
           children: [
             Hero(
-              tag: "main_card",
+              tag: "main_card/deprecated",
               child: Container(
                 height: double.infinity,
                 width: double.infinity,
@@ -45,13 +46,39 @@ class DetailPage extends StatelessWidget {
                     width: cardBorderWidth,
                   ),
                 ),
-                child: Column(
-                  children: const [],
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: headerHeight + 8,
+                      ),
+                      SizedBox(
+                        height: 64,
+                        width: double.infinity,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            const SizedBox(width: 32),
+                            _buildSunriseWidget(Icons.wb_sunny, "12:36 PM", true),
+                            const SizedBox(width: 8),
+                            Expanded(
+                              child: CustomPaint(
+                                painter: ArrowPainer(),
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            _buildSunriseWidget(Icons.dark_mode, "06:18 AM", false),
+                            const SizedBox(width: 32),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
             SizedBox(
-              height: 148,
+              height: headerHeight,
               child: Stack(
                 children: [
                   Hero(
@@ -108,23 +135,26 @@ class DetailPage extends StatelessWidget {
             Positioned(
               top: 0,
               right: 0,
-              child: Container(
-                height: 48,
-                width: 86,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.secondary,
-                  borderRadius: const BorderRadius.only(
-                    topRight: Radius.circular(32),
-                    bottomLeft: Radius.circular(32),
+              child: GestureDetector(
+                onTap: () => Navigator.of(context).pop(),
+                child: Container(
+                  height: 48,
+                  width: 86,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(32),
+                      bottomLeft: Radius.circular(32),
+                    ),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: cardBorderWidth,
+                    ),
                   ),
-                  border: Border.all(
-                    color: Colors.black,
-                    width: cardBorderWidth,
+                  child: const FittedBox(
+                    fit: BoxFit.contain,
+                    child: Icon(Icons.arrow_back_rounded, color: Colors.black),
                   ),
-                ),
-                child: const FittedBox(
-                  fit: BoxFit.contain,
-                  child: Icon(Icons.arrow_back_rounded, color: Colors.black),
                 ),
               ),
             ),
@@ -133,4 +163,68 @@ class DetailPage extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildSunriseWidget(IconData iconData, String text, bool sunrise) {
+    return SizedBox(
+      width: 96,
+      height: 64,
+      child: Row(
+        textDirection: sunrise ? TextDirection.ltr : TextDirection.rtl,
+        children: [
+          // side info
+          SizedBox(
+            width: 48,
+            child: Text(
+              text,
+              style: const TextStyle(fontWeight: FontWeight.w800, fontSize: 14),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          // icon display
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                iconData,
+                color: Colors.black,
+                size: 40,
+              ),
+              Text(
+                sunrise ? "sunrise" : "sunset",
+                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ArrowPainer extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    const double arrowHeightPerc = .2;
+    const double arrowWidthPerc = .9;
+    final double height = size.height;
+    final double widgth = size.width;
+
+    final Paint paint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 4
+      ..strokeCap = StrokeCap.round;
+
+    canvas.drawLine(Offset(0, height / 2), Offset(widgth, height / 2), paint);
+    canvas.drawLine(
+        Offset(widgth, height / 2),
+        Offset(widgth * arrowWidthPerc, height / 2 + height * arrowHeightPerc),
+        paint);
+    canvas.drawLine(
+        Offset(widgth, height / 2),
+        Offset(widgth * arrowWidthPerc, height / 2 - height * arrowHeightPerc),
+        paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
