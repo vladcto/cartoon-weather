6283c7ad11f19d2e432713715692f1f45c336b7c
vladcto <razrab.ytka@gmail.com>
2023-01-23 21:47:02 +0300

    Рефакторинг нижнего бара.

--- cartoon_weather/lib/bottom_bar.dart
+++ cartoon_weather/lib/bottom_bar.dart
@@ -26,68 +26,62 @@ class _BottomBarState extends State<BottomBar> {
       child: SizedBox(
         height: barHeight,
         width: double.infinity,
-        child: Stack(
-          clipBehavior: Clip.none,
-          fit: StackFit.expand,
-          children: [
-            Container(
-              height: barHeight,
-              width: 400,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: Colors.black,
-                  width: barStroke,
-                ),
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(24),
-                ),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black12,
-                    offset: Offset(0, -6),
-                    blurRadius: 6,
-                  ),
-                ],
+        child: Container(
+          height: barHeight,
+          width: 400,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.black,
+              width: barStroke,
+            ),
+            borderRadius: const BorderRadius.vertical(
+              top: Radius.circular(24),
+            ),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(0, -6),
+                blurRadius: 6,
               ),
-              child: Stack(
-                alignment: Alignment.topCenter,
-                fit: StackFit.expand,
-                children: [
-                  Positioned(
-                    top: topSliderMargin,
-                    child: GestureDetector(
-                      onPanUpdate: (details) {
-                        _bottom -= details.delta.dy;
-                        _bottom =
-                            min(barHeight - sliderSize - barStroke, max(0, _bottom));
-                        setState(() {});
-                      },
-                      child: Container(
-                        width: 128,
-                        height: sliderHeight,
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.secondary,
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 2,
-                          ),
-                          borderRadius: BorderRadius.circular(10000),
-                        ),
+            ],
+          ),
+          child: Stack(
+            alignment: Alignment.topCenter,
+            fit: StackFit.expand,
+            children: [
+              Positioned(
+                top: topSliderMargin,
+                child: GestureDetector(
+                  onPanUpdate: (details) {
+                    _bottom -= details.delta.dy;
+                    _bottom =
+                        min(barHeight - sliderSize - barStroke, max(0, _bottom));
+                    setState(() {});
+                  },
+                  child: Container(
+                    width: 128,
+                    height: sliderHeight,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondary,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
                       ),
+                      borderRadius: BorderRadius.circular(10000),
                     ),
                   ),
-                  Positioned(
-                    top: sliderSize,
-                    bottom: 0,
-                    right: 0,
-                    left: 0,
-                    child: widget.child,
-                  ),
-                ],
+                ),
               ),
-            ),
-          ],
+              Positioned(
+                top: sliderSize,
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: widget.child,
+              ),
+            ],
+          ),
         ),
       ),
     );
