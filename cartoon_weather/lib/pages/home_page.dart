b6510ba5052d4c047114b65aa83171e298f62db5
vladcto <razrab.ytka@gmail.com>
2023-01-27 01:43:16 +0300

    Добавил лого яндекса в апп бар. Пока аппбары все равно сменяют друг друга.

--- cartoon_weather/lib/pages/home_page.dart
+++ cartoon_weather/lib/pages/home_page.dart
@@ -22,14 +22,23 @@ class HomePage extends StatelessWidget {
     ThemeImages themeImages = Theme.of(context).extension<ThemeImages>()!;
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text(
-            "Weather Toon",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontFamily: "Inter",
+        title: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const Expanded(
+              child: Text(
+                "Weather Toon",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Inter",
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
+            Expanded(
+              child: YandexLogo(),
+            ),
+          ],
         ),
         shape: const Border(
           bottom: BorderSide(width: 2),
@@ -204,6 +213,38 @@ class HomePage extends StatelessWidget {
               );
             },
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class YandexLogo extends StatelessWidget {
+  const YandexLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 36,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Image.asset(
+              "assets/images/yandex_logo.png",
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          const Text(
+            "Источник данных.",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: "Inter",
+              fontSize: 12,
+            ),
+            textAlign: TextAlign.center,
+          )
         ],
       ),
     );
